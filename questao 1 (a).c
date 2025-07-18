87fc00d2dfa4dbf3c325d0888e3b6f5146de48c5
luidrocha-collab <luidrocha2019@gmail.com>
2025-07-18 07:37:52 -0300

    Create questao 1 (a).c

---
+++ questao 1 (a).c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    printf("**********\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    print#f("*        *\n");
+    printf("**********\n");
+
+    printf("  ***  \n");
+    printf(" *   * \n");
+    printf("*     *\n");
+    printf("*     *\n");
+    printf("*     *\n");
+    printf(" *   * \n");
+    printf("  ***  \n");
+
+    printf("    *    \n");
+    printf("   ***   \n");
+    printf("  *****  \n");
+    printf(" ******* \n");
+    printf("*********\n");
+    printf("    *    \n");
+    printf("    *    \n");
+
+    printf("   *   \n");
+    printf("  * *  \n");
+    printf(" *   * \n");
+    printf("*     *\n");
+    printf(" *   * \n");
+    printf("  * *  \n");
+    printf("   *   \n");
+}
